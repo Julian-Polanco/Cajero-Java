3453b01c84a27d3ae5a2b5b234f4c8531a14af7a
julian020914 <demianflithxd.14@gmail.com>
2021-08-11 20:50:55 -0500

    Update

--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -17,7 +17,6 @@ public class Cajero {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
-        System.out.println("aca yo");
         System.out.println("ultima prueba");
     }
     
