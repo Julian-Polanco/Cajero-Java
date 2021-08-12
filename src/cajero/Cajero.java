10e3fb9c2879efa1803b9181010462c524ee6853
Juan David Salgado Ariza <jdsalgado77@misena.edu.co>
2021-08-12 13:19:55 -0500

    juan
    probando conexion

--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -7,12 +7,6 @@ public class Cajero {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
-        System.out.println("Prueba");
-        System.out.println("nicol");
-        System.out.println("Hola nicol :v");
-        System.out.println("Hola Julian jajaja :v");
-        System.out.println("Hola a todos :v");
-        System.out.println("Cajero");
         System.out.println("Hola Grupo Media Libra de Agua :v");
     }
     
