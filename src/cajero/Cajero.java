662d0780e0e6d6393f451b73334caeea05ab507d
julian020914 <demianflithxd.14@gmail.com>
2021-08-11 20:29:25 -0500

    aca yo
    aca yo

--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -17,6 +17,7 @@ public class Cajero {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("aca yo");
     }
     
 }
