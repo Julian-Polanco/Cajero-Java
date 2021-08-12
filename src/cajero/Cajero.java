aee93291a1ea5b6e6ee099150fa8f7b299f6ad68
julian020914 <demianflithxd.14@gmail.com>
2021-08-12 12:43:03 -0500

    Julian

--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cajero;
 
 /*@author GRUPO MEDIA LIBRA DE AGUA :V */
 
 public class Cajero {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-
         System.out.println("hola mundo");
         System.out.println("Prueba");
         System.out.println("nicol");
@@ -23,7 +14,6 @@ public class Cajero {
         System.out.println("Hola a todos :v");
         System.out.println("Cajero");
         System.out.println("Hola Grupo Media Libra de Agua :v");
-
     }
     
 }
