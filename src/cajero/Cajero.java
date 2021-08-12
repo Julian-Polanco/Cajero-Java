4612a6e878a12941a89085a2359da1d27ccdb1b7
julian020914 <demianflithxd.14@gmail.com>
2021-08-11 22:02:34 -0500

    conexion

--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -17,8 +17,6 @@ public class Cajero {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
-        System.out.println("ultima prueba");
-        System.out.println("Prueba conexion");
         System.out.println("Prueba");
         
     }
