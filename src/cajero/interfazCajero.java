8bc3f50d789ec4930cd06290ef3ac09b537c254e
Estiven <jerodriguez142@misena.edu.co>
2021-08-13 19:47:32 -0500

    Update interfazCajero.java

--- src/cajero/interfazCajero.java
+++ src/cajero/interfazCajero.java
@@ -9,12 +9,12 @@ package cajero;
  *
  * @author estiv
  */
-public class interfazCajero extends javax.swing.JFrame {
+public class interfazcajero extends javax.swing.JFrame {
 
     /**
      * Creates new form interfazcajero
      */
-    public interfazCajero() {
+    public interfazcajero() {
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -116,8 +116,8 @@ public class interfazCajero extends javax.swing.JFrame {
 
     private void btncambiarclaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncambiarclaveActionPerformed
         // TODO add your handling code here:
-        cambioDeClave cambiocla= new cambioDeClave();
-        cambiocla.setVisible(true);
+        cambioDeClave cambioclave= new cambioDeClave();
+        cambioclave.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_btncambiarclaveActionPerformed
 
@@ -138,21 +138,20 @@ public class interfazCajero extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(interfazCajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(interfazcajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(interfazCajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(interfazcajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(interfazCajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(interfazcajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(interfazCajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(interfazcajero.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new interfazCajero().setVisible(true);
+                new interfazcajero().setVisible(true);
             }
         });
     }
