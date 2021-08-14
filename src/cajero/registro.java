7655802daff97a0f59bb2302a34c182562d596a3
Estiven <jerodriguez142@misena.edu.co>
2021-08-13 20:36:39 -0500

    actu2
    34

--- src/cajero/registro.java
+++ src/cajero/registro.java
@@ -179,7 +179,7 @@ public class registro extends javax.swing.JFrame {
     private void btn_registrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_registrarActionPerformed
         // TODO add your handling code here:
           try{
-        PreparedStatement pst = cn.prepareStatement("INSERT INTO usuarios (tipoDoc, numDoc, nombre, correo ,contraseña, telefono, fechaNac, numCuenta,  saldo) VALUES (?,?,?,?,?,?,?,?,0)");    
+        PreparedStatement pst = cn.prepareStatement("INSERT INTO usuarios (tipoDoc, numDoc, nombre, correo ,contraseña, telefono, fechaNac) VALUES (?,?,?,?,?,?,?,?,0)");    
             pst.setString(1, (String) tipo_doc.getSelectedItem());
             pst.setString(2,  num_doc.getText());
             pst.setString(3, nombre.getText());
